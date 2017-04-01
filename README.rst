c7726d41c53dd2d8204b7555cfb2c329193f6a3c
Felix Seele <fseele@gmail.com>
2017-04-01 20:33:06 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+BeaconTools
+===========
+
+A Python Library for working with various types of Bluetooth LE Beacons.
---
+++ setup.py
@@ -0,0 +1,65 @@
+"""Setup module for beacontools."""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='beacontools',
+
+    version='0.0.1',
+
+    description='A Python Library for working with various types of Bluetooth LE Beacons.',
+    long_description=long_description,
+
+    url='https://github.com/citruz/beacontools',
+
+    author='Felix Seele',
+    author_email='fseele@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='beacons ibeacon eddystone bluetooth low energy ble',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    #install_requires=['peppercorn'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+)
