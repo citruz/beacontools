36edb877ee79a2b1947d91468539fdf80c683ebc
Shaun <Shaunplummer247@googlemail.com>
2019-01-29 19:37:50 +0000

    Update Nearable example

--- examples/scanner_estimote_stickers_example.py
+++ examples/scanner_estimote_stickers_example.py
@@ -4,7 +4,7 @@ from beacontools import BeaconScanner, EstimoteFilter
 
 def callback(bt_addr, rssi, packet, additional_info):
     print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
-print "Start"
+
 scanner = BeaconScanner(callback,
                         device_filter=EstimoteFilter(identifier="1efe427eb6f4bc2f")
                         )
