18a1728534791c60970a16b1b3b095b55be82544
Greg V <greg@unrelenting.technology>
2020-06-27 00:24:38 +0300

    Add FreeBSD backend

--- beacontools/backend/__init__.py
+++ beacontools/backend/__init__.py
@@ -2,5 +2,7 @@ import sys
 
 if sys.platform.startswith("linux"):
     from .bluez import *
+elif sys.platform.startswith("freebsd"):
+    from .freebsd import *
 else:
     raise NotImplementedError("Scanning not supported on this platform")
